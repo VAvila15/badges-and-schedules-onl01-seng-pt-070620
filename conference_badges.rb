83a2fe55c4a19f70006eae85a0d09ace15dc965c
Victor Avila <avilavictor1570@gmail.com>
2020-07-14 04:07:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,4 +12,8 @@ end
 
 def assign_rooms (attendees)
   room_assignments = []
-  assign_rooms.each do "Hello, #{name}! You'll be assigned to room !"
+  assign_rooms.each_with_index do |name, index|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{number}!"
+  end
+  room_assignments
+end
