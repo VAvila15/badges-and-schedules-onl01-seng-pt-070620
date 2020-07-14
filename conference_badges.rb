5777e380643fe4c388301e34f157b6a014786abc
Victor Avila <avilavictor1570@gmail.com>
2020-07-14 04:33:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,10 +20,10 @@ end
 
 def printer (attendees)
   batch_badge_creator(attendees).each do |badges|
-    "badge"
+      badge
     end
   
   assign_rooms(attendees).each do |rooms|
-    "rooms"
+    rooms
   end
 end
